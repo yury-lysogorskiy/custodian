635c9e6646349dd4b734c3ee74f88d902ef3e366
Richard Tran <rit634989@gmail.com>
2015-10-26 17:29:35 -0700

    increase AMIX instead, looks like decreasing it only makes dE larger

--- custodian/vasp/handlers.py
+++ custodian/vasp/handlers.py
@@ -450,8 +450,8 @@ class UnconvergedErrorHandler(ErrorHandler):
                                         "AMIX_MAG": 0.8,
                                         "BMIX_MAG": 0.001}}}]
 
-        if algo == "Normal" and amix >= 0.2:
-            actions[1]["action"]["_set"]["AMIX"] = amix/2
+        if algo == "Normal" and amix <= 0.3:
+            actions[1]["action"]["_set"]["AMIX"] = amix*1.5
 
         VaspModder().apply_actions(actions)
         return {"errors": ["Unconverged"], "actions": actions}
